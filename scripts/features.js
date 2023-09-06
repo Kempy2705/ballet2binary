84c72ff82335c13566a670e98c30f21951c9d46d
Dan <drdkempson@live.com.au>
2023-09-06 18:35:23 +1000

    Remove Height Re-Adjustment when Hovering over Fact Div

--- scripts/features.js
+++ scripts/features.js
@@ -10,23 +10,19 @@ let funFact3 = document.getElementById('fact3');
 
 function revealFact() {
     funFact1.textContent = "I've have danced and performed across Australia and internationally in Europe and Asia";
-    funFact1.style.height = "150px";
     funFact1.style.border = 'none';
 
 } 
 function revealFact2() {
     funFact2.textContent = "In addition to programming, I have addtional experience working as a Service Desk Technician";
-    funFact2.style.height = "150px";
     funFact2.style.border = 'none';
 } 
 function revealFact3() {
     funFact3.textContent = "Other hobbies include Films, Live Performance and Indie Video Games";
-    funFact3.style.height = "150px";
     funFact3.style.border = 'none';
 } 
 function restoreMystery(event) {
     event.target.textContent = '?';
-    event.target.style.height = "125px";
     event.target.style.border = "solid 3.5px black"
 }
 
